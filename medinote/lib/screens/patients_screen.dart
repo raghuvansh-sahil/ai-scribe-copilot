09118ef05d2e529290c55a62c70c4f3b4297dcac
raghuvansh-sahil <sahil.raghuvansh.sr@gmail.com>
2025-09-27 16:16:32 +0530

    second commit

--- medinote/lib/screens/patients_screen.dart
+++ medinote/lib/screens/patients_screen.dart
@@ -1,84 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:medinote/models/patient.dart';
-import 'package:medinote/models/session.dart';
-import 'package:medinote/widgets/session_card.dart';
+import 'package:medinote/screens/patient_screen.dart';
+import 'package:medinote/widgets/patient_card.dart';
 
-class PatientScreen extends StatefulWidget {
-  final Patient patient;
-
-  const PatientScreen({super.key, required this.patient});
-
-  @override
-  State<PatientScreen> createState() => _PatientScreenState();
-}
-
-class _PatientScreenState extends State<PatientScreen> {
-  final Set<int> expandedSessions = {};
+class PatientsScreen extends StatelessWidget {
+  const PatientsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.patient.name)),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Text(
-                  widget.patient.name,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(widget.patient.id, style: const TextStyle(fontSize: 15)),
-              ],
-            ),
-            const SizedBox(height: 12),
-            Expanded(
-              child: FutureBuilder<List<Session>>(
-                future: NetworkHandler.getReceipts(),
-                builder: (context, AsyncSnapshot<List<Session>> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return Center(child: Text(snapshot.error.toString()));
-                  } else if (snapshot.hasData && snapshot.data != null) {
-                    final sessions = snapshot.data!;
-                    if (sessions.isEmpty) {
-                      return const Center(child: Text('No sessions saved.'));
-                    }
-                    return ListView.builder(
-                      itemCount: sessions.length,
-                      itemBuilder: (context, index) {
-                        final isExpanded = expandedSessions.contains(index);
-
-                        return GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              if (isExpanded) {
-                                expandedSessions.remove(index);
-                              } else {
-                                expandedSessions.add(index);
-                              }
-                            });
-                          },
-                          child: SessionCard(
-                            session: sessions[index],
-                            showFull: isExpanded,
-                          ),
-                        );
-                      },
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: FutureBuilder<List<Patient>>(
+        future: NetworkHandler.getReceipts(),
+        builder: (context, AsyncSnapshot<List<Patient>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: const CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          } else if (snapshot.hasData && snapshot.data != null) {
+            final patients = snapshot.data!;
+            if (patients.isEmpty) {
+              return const Center(child: Text('No patients added.'));
+            }
+            return ListView.builder(
+              itemCount: patients.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            PatientScreen(patient: patients[index]),
+                      ),
                     );
-                  } else {
-                    return const Center(child: Text('Nothing to show.'));
-                  }
-                },
-              ),
-            ),
-          ],
-        ),
+                  },
+                  child: PatientCard(patient: patients[index]),
+                );
+              },
+            );
+          } else {
+            return const Center(child: Text('Nothing to show.'));
+          }
+        },
       ),
     );
   }
